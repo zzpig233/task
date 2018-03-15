47a1c8f77066ac843b4ae87963fa22b6953d78aa
zzpig <203360009@qq.com>
2018-03-16 03:21:31 +0800

    修复了再次点击按钮浮层不会消失的问题

--- 浮层开关/main.js
+++ 浮层开关/main.js
@@ -1,5 +1,10 @@
 clickMe.addEventListener('click', function(e){
-    popover.style.display = 'block'
+    if(popover.style.display === 'block'){
+        popover.style.display = 'none'
+    }else{
+        popover.style.display = 'block'
+    }
+    
 })
 wrapper.addEventListener('click', function(e){
     e.stopPropagation()
