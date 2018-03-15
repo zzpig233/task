9716ee18385fc6aa04e8d23b7ec4b9b77e3a642d
zzpig <203360009@qq.com>
2018-03-15 21:43:38 +0800

    轮播

---
+++ 轮播/main.js
@@ -0,0 +1,45 @@
+var allButtons = $('#buttons > span')
+
+for (let i = 0; i < allButtons.length; i++){
+    $(allButtons[i]).on('click', function(x){
+        var index = $(x.currentTarget).index()
+        var p = index * -920
+        $('#images').css({
+            transform: 'translate(' + p + 'px)'
+        })
+        n = index
+        activeButton(allButtons.eq(n))
+    })
+}
+
+var n =0;
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click')
+    
+var timerId = setTimer()
+
+$('.window').on('mouseenter', function() {
+    window.clearInterval(timerId)
+})
+$('.window').on('mouseleave', function() {
+    timerId = setTimer()
+})
+
+function setTimer(){
+    return setInterval(() =>{
+        n += 1
+        allButtons.eq(n % size).trigger('click')
+    }, 3000)
+}
+
+function activeButton($button){
+    $button.addClass('active').siblings('.active').removeClass('active')
+}
+
+document.addEventListener('visibilitychange', function(e){
+    if(document.hidden){
+        window.clearInterval(timerId)
+    }else{
+        timerId = setTimer()
+    }
+})
